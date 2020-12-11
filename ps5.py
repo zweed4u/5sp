8675a42a28d808bc90040bf3edad3d5f2299a5d2
Zachary Weeden <zdw7287@rit.edu>
2020-12-10 23:30:17 -0500

    no webhook

--- ps5.py
+++ ps5.py
@@ -6,28 +6,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-class SlackRequester:
-    def __init__(self, url: Optional[str]):
-        self.url = url
-
-    def send_message(self, message: str):
-        slack_response = requests.request("POST", self.url, json={"text": message})
 
 
-slack_requester.send_message(
-    f"<!channel> *{datetime.datetime.now()} UTC* - initializing ps5 bot"
-)
-print(f"[*] {datetime.datetime.now()} :: Parsing cli options")
-parser.add_argument("--poll", type=int, default=60)
-args = parser.parse_args()
-poll = args.poll
+
+poll = 60
 
 print(f"[*] {datetime.datetime.now()} :: Reading config")
-current_directory = os.path.dirname(os.path.realpath(__file__))
-with open(f"{current_directory}/config.json") as json_file:
-    config_data = json.load(json_file)
-slack_webhook_url = config_data.get("webhook_url")
-slack_requester = SlackRequester(slack_webhook_url)
+
 
 print(f"[*] {datetime.datetime.now()} :: Setting up webdriver options")
 options = Options()
@@ -53,9 +38,6 @@ while running:
             print(
                 f"[!] {datetime.datetime.now()} :: 'queue' in url or url has redirected!"
             )
-            slack_requester.send_message(
-                f"<!channel> *{datetime.datetime.now()} UTC* - 'queue' in url or hardlink redirect! - {ps5_url}"
-            )
         try:
             add_btn = driver.find_element_by_class_name(
                 "btn.transparent-orange-button.js-analyitics-tag.add-to-cart"
@@ -68,20 +50,11 @@ while running:
                 print(
                     f"[!] {datetime.datetime.now()} :: ADD TO CART BUTTON IS VISIBLE AND NO REDIRECT - GOGOGO!"
                 )
-                slack_requester.send_message(
-                    f"<!channel> *{datetime.datetime.now()} UTC* - Add to cart button is displayed and hardlink had no redirect - {ps5_url}"
-                )
         except Exception as exc:
             print(f"[*/!] {datetime.datetime.now()} :: Unable to find add button?")
-            slack_requester.send_message(
-                f"<!channel> *{datetime.datetime.now()} UTC* - Add to cart button element couldnt be found? - {ps5_url}"
-            )
         time.sleep(poll)
     except (Exception, KeyboardInterrupt) as exc:
         print(f"[*] {datetime.datetime.now()} :: SIGINT caught - closing browser")
-        slack_requester.send_message(
-            f"<!channel> *{datetime.datetime.now()} UTC* - Exception was raised during runtime - ps5 bot exiting"
-        )
         running = False
         continue
 # driver.close() #raises an exception for some reason? (Failed to establish a new connection)
